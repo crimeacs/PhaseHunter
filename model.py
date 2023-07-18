5f8816c9b221c5abf6106f2f33119163c4f04877
Artemii Novoselov <anovosel@sh02-ln04.stanford.edu>
2023-07-18 01:41:55 -0700

    added processing of continous waveforms

--- model.py
+++ model.py
@@ -1,15 +1,19 @@
 from lightning import seed_everything
 import lightning as pl
 
-import torch
 from masksembles import common
 import torch
-import torch.nn.parallel
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 from torchmetrics import MeanAbsoluteError
 from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+from scipy.stats import gaussian_kde
+from scipy.special import comb
+
+from tqdm.auto import tqdm
+import pandas as pd
 
 seed_everything(42, workers=False)
 torch.set_float32_matmul_precision('medium')
@@ -24,22 +28,9 @@ class BlurPool1D(nn.Module):
         self.stride = stride
         self.off = int((self.stride - 1) / 2.)
         self.channels = channels
-
-        # print('Filter size [%i]' % filt_size)
-        if(self.filt_size == 1):
-            a = np.array([1., ])
-        elif(self.filt_size == 2):
-            a = np.array([1., 1.])
-        elif(self.filt_size == 3):
-            a = np.array([1., 2., 1.])
-        elif(self.filt_size == 4):
-            a = np.array([1., 3., 3., 1.])
-        elif(self.filt_size == 5):
-            a = np.array([1., 4., 6., 4., 1.])
-        elif(self.filt_size == 6):
-            a = np.array([1., 5., 10., 10., 5., 1.])
-        elif(self.filt_size == 7):
-            a = np.array([1., 6., 15., 20., 15., 6., 1.])
+        
+        # Generate coefficients for the specified filter size
+        a = np.array([comb(filt_size-1, i, exact=False) for i in range(filt_size)])
 
         filt = torch.Tensor(a)
         filt = filt / torch.sum(filt)
@@ -64,7 +55,7 @@ def get_pad_layer_1d(pad_type):
     elif(pad_type == 'zero'):
         PadLayer = nn.ZeroPad1d
     else:
-        print('Pad type [%s] not recognized' % pad_type)
+        print(f"Pad type [{pad_type}] not recognized")
     return PadLayer
 
 class Masksembles1D(nn.Module):
@@ -186,71 +177,15 @@ class PhaseHunter(pl.LightningModule):
             # nn.GELU(),
             # nn.GroupNorm(2,512),
         )
-        
+
         self.out = nn.Sequential(
             nn.LazyLinear(n_outs),
             nn.Sigmoid()
         )
 
-
         self.save_hyperparameters(ignore=['picker'])
         self.mae = MeanAbsoluteError()
-
-    def compute_loss(self, y, pick, mae_name=False):
-        y_filt = y[y != 0]
-        pick_filt = pick[y != 0]
-        if len(y_filt) > 0:
-            loss = F.l1_loss(y_filt, pick_filt.flatten())
-            if mae_name != False:
-                mae_phase = self.mae(y_filt, pick_filt.flatten())*120
-                self.log(f'MAE/{mae_name}_val', mae_phase,  on_step=False, on_epoch=True, prog_bar=False)
-        else:
-            loss = 0
-        return loss
-            
-    def training_step(self, batch, batch_idx):
-        # training_step defines the train loop.
-        x, y_p, y_s = batch
         
-        picks = self(x)
-        
-        p_pick  = picks[:,0]
-        s_pick  = picks[:,1]
-
-        p_loss = self.compute_loss(y_p, p_pick, mae_name='P')
-        s_loss = self.compute_loss(y_s, s_pick, mae_name='S')
-
-        loss = (p_loss+s_loss)/self.n_outs
-        
-        self.log('Loss/train', loss, on_step=True, on_epoch=False, prog_bar=True)
-        
-        return loss
-    
-    def validation_step(self, batch, batch_idx):
-        
-        x, y_p, y_s = batch
-        
-        picks = self(x)
-        
-        p_pick  = picks[:,0]
-        s_pick  = picks[:,1]
-
-        p_loss = self.compute_loss(y_p, p_pick, mae_name='P')
-        s_loss = self.compute_loss(y_s, s_pick, mae_name='S')
-
-        loss = (p_loss+s_loss)/self.n_outs
-            
-        self.log('Loss/val',  loss, on_step=False, on_epoch=True, prog_bar=False)
-        
-        return loss
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=3e-4)
-        scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=10, cooldown=10, threshold=1e-3)
-        # scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, 3e-4,  epochs=300, steps_per_epoch=len(train_loader))
-        monitor = 'Loss/train'
-        return {"optimizer": optimizer,  "lr_scheduler": scheduler, 'monitor': monitor}
-    
     def forward(self, x):
         # Feature extraction
         x = self.block1(x)    
@@ -268,7 +203,129 @@ class PhaseHunter(pl.LightningModule):
         x = self.block9(x)
         
         # Regressor
-        x = x.flatten(start_dim=1)
-        x = self.out(x)
+        embedding = x.flatten(start_dim=1)
+        x = self.out(embedding)
         
-        return x
+        return x, embedding
+        
+    def compute_loss(self, y, pick, mae_name=False):
+        y_filt = y[y != 0]
+        pick_filt = pick[y != 0]
+        if len(y_filt) > 0:
+            loss = F.l1_loss(y_filt, pick_filt.flatten())
+            if mae_name != False:
+                mae_phase = self.mae(y_filt, pick_filt.flatten())*120
+                self.log(f'MAE/{mae_name}_val', mae_phase,  on_step=False, on_epoch=True, prog_bar=False)
+        else:
+            loss = 0
+        return loss
+
+    def get_likely_val(self, array):
+        kde = gaussian_kde(array)
+        dist_space = np.linspace(min(array)-0.001, max(array)+0.001, 512)
+        val = torch.tensor(dist_space[np.argmax(kde(dist_space))], dtype=torch.float32)
+        uncertainty = dist_space.ptp()/2
+        return dist_space, kde, val, uncertainty
+
+    def process_continuous_waveform(self, st):
+        # Get time of the stream
+        start_time = st[0].stats.starttime
+        end_time = st[0].stats.endtime
+        
+        # Define the chunk size in seconds
+        chunk_size = 60
+        
+        # Define the array to store chunks
+        chunks = []
+        
+        predictions = pd.DataFrame()
+        
+        # Loop over the time range with the step equal to chunk_size
+        for chunk_start in tqdm(np.arange(start_time, end_time, chunk_size)):
+            # Define the chunk end time
+            chunk_end = chunk_start + chunk_size
+        
+            # Slice the stream
+            chunk = st.slice(chunk_start, chunk_end)
+            chunk = np.vstack([x.data for x in chunk], dtype='float')[:,:-1]
+            chunk -= chunk.mean(axis=0)
+
+            # Normalize
+            max_val = np.max(np.abs(chunk))
+            chunk = chunk/max_val
+
+            chunk = torch.tensor(chunk, dtype=torch.float)
+        
+            inference_sample = torch.stack([chunk]*128).cuda()
+            
+            with torch.no_grad():
+                preds, embeddings = self(inference_sample)
+                p_pred = preds[:,0].detach().cpu()
+                s_pred = preds[:,1].detach().cpu()
+                embeddings = torch.mean(embeddings, axis=0).detach().cpu().numpy()
+        
+                p_dist, p_kde, p_val, p_uncert = self.get_likely_val(p_pred)
+                s_dist, s_kde, s_val, s_uncert = self.get_likely_val(s_pred)
+        
+                p_time = chunk_start+p_val.item()*chunk_size
+                s_time = chunk_start+s_val.item()*chunk_size
+        
+                predictions = predictions.append({'p_time': p_time, 's_time':s_time,
+                                    'p_uncert' : p_uncert, 's_uncert' : s_uncert,
+                                    'embedding' : embeddings}, ignore_index=True)
+        
+        predictions['p_conf'] = 1/predictions['p_uncert']
+        predictions['s_conf'] = 1/predictions['s_uncert']
+        predictions['p_conf'] /= predictions['p_conf'].max()
+        predictions['s_conf'] /= predictions['s_conf'].max()
+        
+        predictions['p_time_rel'] = (predictions.p_time.apply(lambda x: pd.Timestamp(x.timestamp, unit='s')) - pd.Timestamp(predictions.p_time.iloc[0].date)).dt.total_seconds()
+        
+        predictions['s_time_rel'] = (predictions.s_time.apply(lambda x: pd.Timestamp(x.timestamp, unit='s')) - pd.Timestamp(predictions.s_time.iloc[0].date)).dt.total_seconds()
+        return predictions
+
+            
+    def training_step(self, batch, batch_idx):
+        # training_step defines the train loop.
+        x, y_p, y_s = batch
+        
+        picks, embedding = self(x)
+        
+        p_pick  = picks[:,0]
+        s_pick  = picks[:,1]
+
+        p_loss = self.compute_loss(y_p, p_pick, mae_name='P')
+        s_loss = self.compute_loss(y_s, s_pick, mae_name='S')
+
+        loss = (p_loss+s_loss)/self.n_outs
+        
+        self.log('Loss/train', loss, on_step=True, on_epoch=False, prog_bar=True)
+        
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        
+        x, y_p, y_s = batch
+        
+        picks, embedding = self(x)
+        
+        p_pick  = picks[:,0]
+        s_pick  = picks[:,1]
+
+        p_loss = self.compute_loss(y_p, p_pick, mae_name='P')
+        s_loss = self.compute_loss(y_s, s_pick, mae_name='S')
+
+        loss = (p_loss+s_loss)/self.n_outs
+            
+        self.log('Loss/val',  loss, on_step=False, on_epoch=True, prog_bar=False)
+        
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=3e-5)
+        scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=10, cooldown=10, threshold=1e-6)
+        # scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, 3e-4,  epochs=300, steps_per_epoch=len(train_loader))
+        monitor = 'Loss/train'
+        return {"optimizer": optimizer,  "lr_scheduler": scheduler, 'monitor': monitor}
+    
+    
